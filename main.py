84dcb39e67ee52ad48ba339dc6c5fb45bac2a786
pj-web <insta.pj.05@gmail.com>
2023-04-22 22:43:37 +0400

    20230422

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-# https://vk.com/@realpythonru-pip
-# https://docs.aiogram.dev/en/dev-3.x/install.html
+from aiogram import Bot, Dispatcher, types
+import asyncio
+token = "6103193076:AAGXfD0e6ChwJTcKZ4dQop5bbeYXz7Q0Uv0"
 
-token = '6103193076:AAGXfD0e6ChwJTcKZ4dQop5bbeYXz7Q0Uv0'
+
+async def echo(message: types.Message):
+    print(message.text)
+    await message.answer(message.text)
+
+
+async def start():
+    bots = Bot(token)
+    dp = Dispatcher()
+
+    dp.message.register(echo)
+
+    await dp.start_polling(bots)
+
+if __name__ == "__main__":
+    asyncio.run(start())
